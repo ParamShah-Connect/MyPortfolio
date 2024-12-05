a02e94d9b734db6636047507ce513595af26ae02
Param Shah <param.sanjay.shah@gmail.com>
2024-12-05 12:16:27 +0530

    visits number trackeing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,52 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Comp1 from "../src/components/comp1.jsx";
-import Navbar from "./components/navbar.jsx"; 
+import Navbar from "./components/navbar.jsx";
 import Skills from "./components/skills.jsx";
 import Contact from "./components/contact.jsx";
 import Projects from "./components/projects.jsx";
 import Lastdiv from "./components/lastdiv.jsx";
 import Projectdesc from "./components/projectdesc.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  useEffect(() => {
+    let visits = localStorage.getItem("visits");
+    console.log("total visits: ",visits)
+    if (visits) {
+      localStorage.setItem("visits", parseFloat(visits) + 0.5); 
+    } else {
+      localStorage.setItem("visits", 1); 
+    }
+  }, []);
+  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
-          <>
-            <div id="navbar">
-              <Navbar />
-            </div>
-            <div id="about">
-              <Comp1 />
-            </div>
-            <div id="skills">
-              <Skills />
-            </div>
-            <div id="projects">
-              <Projects />
-            </div>
-            <div id="contact">
-              <Contact />
-            </div>
-            <Lastdiv />
-          </>
-        } />
+        <Route
+          path="/"
+          element={
+            <>
+              <div id="navbar">
+                <Navbar />
+              </div>
+              <div id="about">
+                <Comp1 />
+              </div>
+              <div id="skills">
+                <Skills />
+              </div>
+              <div id="projects">
+                <Projects />
+              </div>
+              <div id="contact">
+                <Contact />
+              </div>
+              <Lastdiv />
+            </>
+          }
+        />
 
-	    <Route path="/projects/:projectname" element={<Projectdesc />} />
+        <Route path="/projects/:projectname" element={<Projectdesc />} />
       </Routes>
     </BrowserRouter>
   );
