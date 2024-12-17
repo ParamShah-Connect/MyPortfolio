53063c763c44b63be7e66655345f7e2d8a41aef2
Param Shah <param.sanjay.shah@gmail.com>
2024-12-17 19:13:34 +0530

    done

--- server/controllers/logics.js
+++ server/controllers/logics.js
@@ -8,7 +8,6 @@ export default async function sendMessage(req, res) {
     if (!name || !email || !subject || !message) {
       return res.status(400).json({ error: "All fields are required." });
     }
-  
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     if (!emailRegex.test(email)) {
       return res.status(400).json({ error: "Invalid email format." });
